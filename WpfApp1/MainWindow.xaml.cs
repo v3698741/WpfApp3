f49e610a4aa9425ac09fab8b98b7f7db57e3b01b
user <user>
2021-03-29 12:56:28 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            
+            myInkCanvas.Strokes.Clear();
+        }
+
+        
+        private void cboBrushSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cboBrushSize.Items.Count > 0 && ((ComboBoxItem)cboBrushSize.SelectedItem).Content != null)
+            {
+               
+                myInkCanvas.DefaultDrawingAttributes.Width = Convert.ToDouble(((ComboBoxItem)cboBrushSize.SelectedItem).Content);
+                myInkCanvas.DefaultDrawingAttributes.Height = Convert.ToDouble(((ComboBoxItem)cboBrushSize.SelectedItem).Content);
+            }
+        }
+
+        private void cboColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cboColor.Items.Count > 0 && ((ComboBoxItem)cboColor.SelectedItem).Content != null)
+            {
+              
+                if (((ComboBoxItem)cboColor.SelectedItem).Content.ToString() == "Black")
+            myInkCanvas.DefaultDrawingAttributes.Color = Colors.Black;
+        else if (((ComboBoxItem)cboColor.SelectedItem).Content.ToString() == "Blue")
+            myInkCanvas.DefaultDrawingAttributes.Color = Colors.Blue;
+        else if (((ComboBoxItem)cboColor.SelectedItem).Content.ToString() == "Green")
+            myInkCanvas.DefaultDrawingAttributes.Color = Colors.Green;
+        else if (((ComboBoxItem)cboColor.SelectedItem).Content.ToString() == "Red")
+            myInkCanvas.DefaultDrawingAttributes.Color = Colors.Red;
+        else if (((ComboBoxItem)cboColor.SelectedItem).Content.ToString() == "Yellow")
+            myInkCanvas.DefaultDrawingAttributes.Color = Colors.Yellow;
+            }
+        }
+
+        private void RadMode_Checked(object sender, RoutedEventArgs e)
+        {
+          
+            if (radDrawingMode.IsChecked.Value == true)
+                myInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            else if (radSelectMode.IsChecked.Value == true)
+                myInkCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+        }
+    }
+}
---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtEdito_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("О разработчике");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("О разработчике");
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            X.Background = Brushes.Red;
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            X.Background = Brushes.Green;
+        }
+
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+            X.Background = Brushes.Blue;
+        }
+
+        private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            X.Background = Brushes.Red;
+        }
+
+        private void ComboBoxItem_Selected_1(object sender, RoutedEventArgs e)
+        {
+            X.Background = Brushes.Green;
+        }
+
+        private void ComboBoxItem_Selected_2(object sender, RoutedEventArgs e)
+        {
+            X.Background = Brushes.Blue;
+        }
+    }
+}
